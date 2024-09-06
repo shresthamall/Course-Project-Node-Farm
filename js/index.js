27e598fcac1b45700b4fad2847e84280f9064b29
shresthamall <shresthaubc@gmail.com>
2024-09-06 20:19:42 +0530

    implemented ESM instead of CJS

--- js/index.js
+++ js/index.js
@@ -1,27 +1,32 @@
 // import fs from 'fs';
-import { ENCODING } from './js/config';
-import { CONTENT_HTML } from './js/config';
-import { CONTENT_JSON } from './js/config';
+import { ENCODING } from './config.js';
+import { CONTENT_HTML } from './config.js';
+import { CONTENT_JSON } from './config.js';
+import { STATUS_NOT_FOUND } from './config.js';
+import { STATUS_OK } from './config.js';
+import * as fs from 'fs';
+import * as http from 'http';
+import * as path from 'path';
+import * as _url from 'url';
 
-const fs = require('fs');
-const http = require('http');
-const path = require('path');
-const url = require('url');
+const __dirname = path.dirname(
+  _url.fileURLToPath(new URL('../node_farm', import.meta.url))
+);
 
 //////////////////////////
 // HTML templates
 const templateOverview = fs.readFileSync(
-  `${__dirname}/template/template-overview.html`,
+  `${__dirname}/templates/template-overview.html`,
   ENCODING
 );
 
 const templateProduct = fs.readFileSync(
-  `${__dirname}/template/template-product.html`,
+  `${__dirname}/templates/template-product.html`,
   ENCODING
 );
 
 const templateCard = fs.readFileSync(
-  `${__dirname}/template/template-card.html`,
+  `${__dirname}/templates/template-card.html`,
   ENCODING
 );
 
@@ -32,22 +37,26 @@ const server = http.createServer((req, res) => {
   const pathName = req.url;
   // Overview page
   if (pathName === '/' || pathName === 'overview') {
-    res.end('This is the Overview');
+    res.writeHead(STATUS_OK, CONTENT_HTML);
+    res.end(templateOverview);
     // Product page
   } else if (pathName === 'product') {
     res.end('This is the product');
   } else if (pathName === '/api') {
-    fs.readFile(`${__dirname}/dev-data/data.json`, ENCODING, (err, data) => {
+    fs.readFile(`${__dirname}/../dev-data/data.json`, ENCODING, (err, data) => {
       if (err) return alert('Could not read file');
       const productData = JSON.parse(data);
       //   console.log(productData);
-      res.writeHead(200, CONTENT_JSON);
+      res.writeHead(STATUS_OK, { 'Content-type': 'application/json' });
       res.end(data);
     });
   }
   // Page not found
   else {
-    res.writeHead(404, CONTENT_HTML);
+    res.writeHead(STATUS_NOT_FOUND, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
+    });
   }
 });
 
