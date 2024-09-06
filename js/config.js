b3f1a05f01c6f6de2f38ac513d493898d3b06d26
shresthamall <shresthaubc@gmail.com>
2024-09-07 02:41:37 +0530

    implemented product page

--- js/config.js
+++ js/config.js
@@ -1,3 +1,5 @@
+import * as path from 'path';
+import * as _url from 'url';
 export const ENCODING = 'utf-8';
 export const CONTENT_HTML = {
   'Content-type': 'text/html',
@@ -7,3 +9,9 @@ export const CONTENT_JSON = {
 };
 export const STATUS_OK = 200;
 export const STATUS_NOT_FOUND = 404;
+
+//////////////////////////
+// Initialize __dirname for ES Module
+export const __dirname = path.dirname(
+  _url.fileURLToPath(new URL('../node_farm', import.meta.url))
+);
--- js/index.js
+++ js/index.js
@@ -6,14 +6,9 @@ import { STATUS_NOT_FOUND } from './config.js';
 import { STATUS_OK } from './config.js';
 import * as fs from 'fs';
 import * as http from 'http';
-import * as path from 'path';
 import * as _url from 'url';
+import { __dirname } from './config.js';
 
-//////////////////////////
-// Initialize __dirname for ES Module
-const __dirname = path.dirname(
-  _url.fileURLToPath(new URL('../node_farm', import.meta.url))
-);
 // HTML templates
 const templateOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -29,22 +24,25 @@ const templateCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
   ENCODING
 );
-console.log(templateCard);
 
 // Data
 // [dataObjects]
 const dataObj = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data.json`, ENCODING)
 );
-console.log(typeof dataObj);
 
 /* 
+following ++ in template-card
 {%PRODUCT%}
 {%IMAGE%}
 {%NOT_ORGANIC%}
 {%QUANTITY%}
 {%PRICE%}
 {%ID%}
+following ++ in template-product
+{%NUTRIENTS%}
+{%FROM%}
+{%DESCRIPTION%}
 */
 
 // Helper functions
@@ -55,45 +53,79 @@ const replaceTemplate = function (template, product) {
     .replace(/{%NOT_ORGANIC%}/g, `${product.organic ? '' : 'not-organic'}`)
     .replace(/{%QUANTITY%}/g, product.quantity)
     .replace(/{%PRICE%}/g, product.price)
-    .replace(/{%ID%}/g, product.id);
-  console.log(product.id, product.image, markup);
+    .replace(/{%ID%}/g, product.id)
+    .replace(/{%NUTRIENTS%}/g, product.nutrients)
+    .replace(/{%FROM%}/g, product.from)
+    .replace(/{%DESCRIPTION%}/g, product.description);
+  // console.log(product.id, product.image, markup);
   return markup;
 };
 
-// Server
-const server = http.createServer((req, res) => {
-  const pathName = req.url;
+// Router
+
+// Returns markup for Overview
+const routeOverviewMarkup = function () {
+  const cardsHTML = dataObj
+    .map(product => replaceTemplate(templateCard, product))
+    .join('');
+
+  return templateOverview.replace(/{%PRODUCT_CARDS%}/, cardsHTML);
+};
+
+// Returns markup for Product with id specified as argument
+const routeProductMarkup = function (productId) {
+  const productObject = dataObj.find(product => product.id === +productId);
+  return replaceTemplate(templateProduct, productObject);
+};
+
+// Route according to requestUrl
+const route = function (requestUrl, res) {
+  const { query, pathname: pathName } = requestUrl;
+  console.log(dataObj.some(product => product.id === +requestUrl.query.id));
+
   // Overview page
-  if (pathName === '/' || pathName === 'overview') {
+  if (pathName === '/' || pathName === '/overview') {
     // HTML markup for product cards
-    const cardsHTML = dataObj
-      .map(product => replaceTemplate(templateCard, product))
-      .join('');
-    const markupCards = templateOverview.replace(
-      /{%PRODUCT_CARDS%}/,
-      cardsHTML
-    );
+    const markupCards = routeOverviewMarkup();
     res.writeHead(STATUS_OK, CONTENT_HTML);
     res.end(markupCards);
-    // Product page
-  } else if (pathName === 'product') {
-    res.end('This is the product');
-  } else if (pathName === '/api') {
-    fs.readFile(`${__dirname}/../dev-data/data.json`, ENCODING, (err, data) => {
-      if (err) return;
-      const productData = JSON.parse(data);
-      //   console.log(productData);
-      res.writeHead(STATUS_OK, { 'Content-type': 'application/json' });
-      res.end(data);
-    });
+    return;
   }
+  // Product page
+  if (
+    pathName === '/product' &&
+    dataObj.some(product => product.id === +query.id)
+  ) {
+    const markupProduct = routeProductMarkup(query.id);
+    res.writeHead(STATUS_OK, CONTENT_HTML);
+    res.end(markupProduct);
+    return;
+  }
+  // if (pathName === '/api') {
+  //   fs.readFile(`${__dirname}/../dev-data/data.json`, ENCODING, (err, data) => {
+  //     if (err) return;
+  //     const productData = JSON.parse(data);
+  //     res.writeHead(STATUS_OK, { 'Content-type': 'application/json' });
+  //     res.end(data);
+  //   });
+  // }
   // Page not found
   else {
+    console.log('else statement executed');
     res.writeHead(STATUS_NOT_FOUND, {
       'Content-type': 'text/html',
       'my-own-header': 'hello-world',
     });
+    res.end();
   }
+};
+
+// Server
+const server = http.createServer((req, res) => {
+  // Request url
+  const requestUrl = _url.parse(req.url, true);
+  // console.log(requestUrl);
+  route(requestUrl, res);
 });
 
 server.listen(8000, () => {
