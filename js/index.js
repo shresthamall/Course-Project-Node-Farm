b5c3c4a13e1cb376f097be937b9f2a25f8d6c1ff
shresthamall <shresthaubc@gmail.com>
2024-09-07 01:02:26 +0530

    Overview implemented

--- js/index.js
+++ js/index.js
@@ -9,11 +9,11 @@ import * as http from 'http';
 import * as path from 'path';
 import * as _url from 'url';
 
+//////////////////////////
+// Initialize __dirname for ES Module
 const __dirname = path.dirname(
   _url.fileURLToPath(new URL('../node_farm', import.meta.url))
 );
-
-//////////////////////////
 // HTML templates
 const templateOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -29,22 +29,58 @@ const templateCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
   ENCODING
 );
+console.log(templateCard);
+
+// Data
+// [dataObjects]
+const dataObj = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data.json`, ENCODING)
+);
+console.log(typeof dataObj);
+
+/* 
+{%PRODUCT%}
+{%IMAGE%}
+{%NOT_ORGANIC%}
+{%QUANTITY%}
+{%PRICE%}
+{%ID%}
+*/
 
 // Helper functions
+const replaceTemplate = function (template, product) {
+  const markup = template
+    .replace(/{%PRODUCT%}/g, product.productName)
+    .replace(/{%IMAGE%}/g, product.image)
+    .replace(/{%NOT_ORGANIC%}/g, `${product.organic ? '' : 'not-organic'}`)
+    .replace(/{%QUANTITY%}/g, product.quantity)
+    .replace(/{%PRICE%}/g, product.price)
+    .replace(/{%ID%}/g, product.id);
+  console.log(product.id, product.image, markup);
+  return markup;
+};
 
 // Server
 const server = http.createServer((req, res) => {
   const pathName = req.url;
   // Overview page
   if (pathName === '/' || pathName === 'overview') {
+    // HTML markup for product cards
+    const cardsHTML = dataObj
+      .map(product => replaceTemplate(templateCard, product))
+      .join('');
+    const markupCards = templateOverview.replace(
+      /{%PRODUCT_CARDS%}/,
+      cardsHTML
+    );
     res.writeHead(STATUS_OK, CONTENT_HTML);
-    res.end(templateOverview);
+    res.end(markupCards);
     // Product page
   } else if (pathName === 'product') {
     res.end('This is the product');
   } else if (pathName === '/api') {
     fs.readFile(`${__dirname}/../dev-data/data.json`, ENCODING, (err, data) => {
-      if (err) return alert('Could not read file');
+      if (err) return;
       const productData = JSON.parse(data);
       //   console.log(productData);
       res.writeHead(STATUS_OK, { 'Content-type': 'application/json' });
